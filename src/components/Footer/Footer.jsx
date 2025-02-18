19cfeba4665761c8b0bd4e6a23bd0b62fb1a5aea
Pasindu Pabasara <pabasarapasindu365@gmail.com>
2025-02-18 18:45:57 +0530

    Add animation to footer links for unique hover effects

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,38 +12,26 @@ export default function Footer() {
                         <p className="text-sm text-gray-400">Email: <a href="mailto:info@seekersconnect.com" className="hover:text-white">info@seekersconnect.com</a></p>
                     </div>
 
-                    <div className="flex flex-col md:flex-row space-y-4 md:space-y-0">
-                        <div className="space-y-4 pr-30">
+                    <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-12">
+                        <div className="space-y-4">
                             <h3 className="font-semibold text-lg text-white">Job Seekers</h3>
-                            <a href="#register" className="text-gray-400 hover:text-white">Register Now</a> <br />
-                            <a href="#search" className="text-gray-400 hover:text-white">Search Jobs</a> <br />
-                            <a href="#login" className="text-gray-400 hover:text-white">Login</a>
+                            <a href="#register" className="text-gray-400 hover:text-white transform transition-all duration-500 hover:translate-x-3 hover:scale-110">Register Now</a> <br />
+                            <a href="#search" className="text-gray-400 hover:text-white transform transition-all duration-500 hover:translate-x-3 hover:scale-110">Search Jobs</a> <br />
+                            <a href="#login" className="text-gray-400 hover:text-white transform transition-all duration-500 hover:translate-x-3 hover:scale-110">Login</a>
                         </div>
-                        <div className="space-y-4 pr-20">
+                        <div className="space-y-4">
                             <h3 className="font-semibold text-lg text-white">Employers</h3>
-                            <a href="#post-job" className="text-gray-400 hover:text-white">Post a Job</a> <br />
-                            <a href="#advertise" className="text-gray-400 hover:text-white">Advertise</a>  <br />
-                            <a href="#terms" className="text-gray-400 hover:text-white">Terms of Service</a>
+                            <a href="#post-job" className="text-gray-400 hover:text-white transform transition-all duration-500 hover:translate-x-3 hover:scale-110">Post a Job</a> <br />
+                            <a href="#advertise" className="text-gray-400 hover:text-white transform transition-all duration-500 hover:translate-x-3 hover:scale-110">Advertise</a>  <br />
+                            <a href="#terms" className="text-gray-400 hover:text-white transform transition-all duration-500 hover:translate-x-3 hover:scale-110">Terms of Service</a>
                         </div>
                         <div className="space-y-4">
                             <h3 className="font-semibold text-lg text-white">Quick Links</h3>
-                            <a href="#about-us" className="text-gray-400 hover:text-white">About Us</a>  <br />
-                            <a href="#contact-us" className="text-gray-400 hover:text-white">Contact Us</a>  <br />
-                            <a href="#faq" className="text-gray-400 hover:text-white">FAQ</a>
+                            <a href="#about-us" className="text-gray-400 hover:text-white transform transition-all duration-500 hover:translate-x-3 hover:scale-110">About Us</a>  <br />
+                            <a href="#contact-us" className="text-gray-400 hover:text-white transform transition-all duration-500 hover:translate-x-3 hover:scale-110">Contact Us</a>  <br />
+                            <a href="#faq" className="text-gray-400 hover:text-white transform transition-all duration-500 hover:translate-x-3 hover:scale-110">FAQ</a>
                         </div>
                     </div>
-                </div>
-
-                <div className="mt-6 flex justify-center space-x-6">
-                    <a href="https://facebook.com" target="_blank" className="text-gray-400 hover:text-white">
-                        <i className="fab fa-facebook-f"></i>
-                    </a>
-                    <a href="https://twitter.com" target="_blank" className="text-gray-400 hover:text-white">
-                        <i className="fab fa-twitter"></i>
-                    </a>
-                    <a href="https://linkedin.com" target="_blank" className="text-gray-400 hover:text-white">
-                        <i className="fab fa-linkedin-in"></i>
-                    </a>
                 </div>
 
                 <div className="text-center mt-6 text-sm text-gray-400">
