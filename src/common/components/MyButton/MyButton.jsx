133cb3915c6670ea49a0186956ef352e5c34811b
Pasindu Pabasara <pabasarapasindu365@gmail.com>
2025-02-18 15:47:48 +0530

    change NavBar,MyButton and HeroSection

--- src/common/components/MyButton/MyButton.jsx
+++ src/common/components/MyButton/MyButton.jsx
@@ -1,20 +1,16 @@
 export default function MyButton(props) {
     return (
         <div>
-            {/* <button
-                class="px-4 py-1 font-semibold rounded-2xl shadow-md cursor-pointer"
-                style={{ background: props.backgroundColor, color: props.txtColor }}>
-                {props.name}
-            </button> */}
 
             <button
-                className="px-4 py-1 font-semibold rounded-2xl shadow-md cursor-pointer"
+                className="px-4 py-1 font-semibold shadow-md cursor-pointer"
                 style={{
                     background: props.backgroundColor,
                     color: props.txtColor,
                     fontSize: props.fontSize,
                     width: props.width,
-                    height: props.height
+                    height: props.height,
+                    borderRadius: props.radius
                 }}
             >
                 {props.name}
