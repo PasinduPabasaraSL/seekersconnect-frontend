5331f7c8768d85b1e3d4f548a3b255cb13111311
Pasindu Pabasara <pabasarapasindu365@gmail.com>
2025-02-23 17:37:44 +0530

    create seperate signin for admin

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -9,22 +9,24 @@ import { BrowserRouter as Router, Route, Routes } from "react-router"
 import SignUp from '../pages/SignUp/SignUp'
 import SignIn from '../pages/SignIn/SignIn'
 import AdminPage from '../pages/AdminPage/AdminPage'
+import AdminSignIn from '../pages/AdminSignIn/AdminSignIn'
 
 function App() {
 
   return (
     <div>
       <Router>
-        {/* <NavBar /> */}
+        <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/jobs" element={<JobPage />} />
           <Route path="/why" element={<WhySeekersConnect />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/sign-in' element={<SignIn />} />
-          <Route path='/admin-page' element={<AdminPage />} />
+          <Route path='/admin/dashboard' element={<AdminPage />} />
+          <Route path='/admin' element={<AdminSignIn />} />
         </Routes>
-        {/* <Footer /> */}
+        <Footer />
       </Router>
     </div>
   )
